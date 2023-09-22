a746277854bce090dea68d45aaef0fbc4c419c59
Alex <alexanderesolis@gmail.com>
2023-09-22 19:15:16 -0400

    Add list of colors to Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,9 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.library.Color;
+
 public final class Constants {
     public static final class DriveConstants{
         public static final double driveMaxSpeed = 0.8;
         public static final boolean doINeedMoreConstants = false;
         public static final double wheelDiameter = 96/25.4;
+    }
+
+    public static final class Colors {
+        public static final Color RED = new Color(235, 54, 26);
+        public static final Color ORANGE = new Color(235, 101, 26);
+        public static final Color YELLOW = new Color(230, 208, 13);
+        public static final Color GREEN = new Color(41, 201, 32);
+        public static final Color BLUE = new Color(32, 125, 201);
+        public static final Color PURPLE = new Color(137, 32, 201);
+        public static final Color WHITE = new Color(255, 255, 255);
+        public static final Color BLACK = new Color(0, 0, 0);
+        public static final Color OVERCLOCKED = new Color(235, 19, 139);
     }
 }
