56ff0836e5588991a55036bdbe07c3c673240028
zach <zachattack531@gmail.com>
2023-10-09 19:52:49 -0400

    worked more on auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed1Side.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed1Side.java
@@ -3,15 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.commands.DriveDistanceCmd;
 import org.firstinspires.ftc.teamcode.commands.TurnCmd;
-import org.firstinspires.ftc.teamcode.subsystems.ImuSub;
 import org.firstinspires.ftc.teamcode.subsystems.DrivetrainSub;
+import org.firstinspires.ftc.teamcode.subsystems.ImuSub;
 
-@Autonomous(name = "Autonomous Red 1 Stage")
-public class AutoRed1 extends CommandOpMode
+@Autonomous(name = "Autonomous Red 1 Side")
+public class AutoRed1Side extends CommandOpMode
 {
     private DrivetrainSub drive;
     private DriveDistanceCmd driveHalfTileLength;
@@ -26,14 +25,14 @@ public class AutoRed1 extends CommandOpMode
     private TurnCmd turnCmd165CW;
     private TurnCmd turnCmd180;
 
-    private double turnSpeed = 0.4;
+    private double turnSpeed = 0.3;
     private double driveSpeed = 0.5;
 
     private ImuSub imu;
 
     private boolean fieldCentric = true;
     @Override
-    public void initialize(){
+    public void initialize() {
         //Initalizing Hardware
         drive = new DrivetrainSub(hardwareMap, telemetry);
         imu = new ImuSub(hardwareMap, telemetry);
@@ -41,30 +40,33 @@ public class AutoRed1 extends CommandOpMode
 
         //Setting up Cmds
         driveHalfTileLength = new DriveDistanceCmd(12, driveSpeed, drive, telemetry);
-        drive20  = new DriveDistanceCmd(20, driveSpeed, drive, telemetry);
+        drive20 = new DriveDistanceCmd(20, driveSpeed, drive, telemetry);
         driveTileLength = new DriveDistanceCmd(24, driveSpeed, drive, telemetry);
         drive3TileLength = new DriveDistanceCmd(72, driveSpeed, drive, telemetry);
-        turnCmd10CW = new TurnCmd(-10,turnSpeed,drive,imu,telemetry);
-        turnCmd75CW = new TurnCmd(-75,turnSpeed,drive,imu,telemetry);
-        turnCmd75CCW = new TurnCmd(75,turnSpeed,drive,imu,telemetry);
-        turnCmd90CCW = new TurnCmd(90,turnSpeed,drive,imu,telemetry);
-        turnCmd165CW = new TurnCmd(-165,turnSpeed,drive,imu,telemetry);
-        turnCmd180 = new TurnCmd(180,turnSpeed,drive,imu,telemetry);
+        turnCmd10CW = new TurnCmd(-10, turnSpeed, drive, imu, telemetry);
+        turnCmd75CW = new TurnCmd(-75, turnSpeed, drive, imu, telemetry);
+        turnCmd75CCW = new TurnCmd(75, turnSpeed, drive, imu, telemetry);
+        turnCmd90CCW = new TurnCmd(86, turnSpeed, drive, imu, telemetry);
+        turnCmd165CW = new TurnCmd(-165, turnSpeed, drive, imu, telemetry);
+        turnCmd180 = new TurnCmd(180, turnSpeed, drive, imu, telemetry);
 
 
         waitForStart();
         schedule(new SequentialCommandGroup(
                 driveTileLength
-                ,turnCmd75CCW
-                ,turnCmd75CW
-                ,driveTileLength
-                ,turnCmd90CCW
+                , turnCmd75CCW
+                , turnCmd75CW
+                ,new DriveDistanceCmd(23, driveSpeed, drive, telemetry)
+                , turnCmd90CCW
                 ,new DriveDistanceCmd(16, driveSpeed, drive, telemetry)
-                ,turnCmd180
-                ,drive3TileLength
-                ,new TurnCmd(-45,turnSpeed,drive,imu,telemetry)
-                ,driveHalfTileLength
-                ));
+                ,turnCmd90CCW
+                ,new DriveDistanceCmd(50, driveSpeed, drive, telemetry)
+                ,turnCmd90CCW
+                ,new DriveDistanceCmd(96, driveSpeed, drive, telemetry)
+                //,new DriveDistanceCmd(96, driveSpeed, drive, telemetry)
+                //,new TurnCmd(-45,turnSpeed,drive,imu,telemetry)
+                //,driveHalfTileLength
+        ));
 
-    }
-}
+
+    }}
